60d6f05fdf38b306f625f65bbe3ca885dc91104d
shiel1dm <shield55@msu.edu>
2021-10-14 14:30:10 -0400

    added some handlebar pages. No css yet. Working on login/auth first.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session')
 const routes = require('./controllers')
+const handlebars = require('express-handlebars')
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
@@ -20,10 +21,20 @@ const sess = {
 
 app.use(session(sess));
 
+app.set('view engine', 'handlebars')
+app.engine('handlebars', handlebars({
+  layoutsDir: __dirname + '/views/layouts'
+}))
+
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //app.use(routes);
+
+app.get('/', (req,res) => {
+  res.render('signup', {layout : 'index'})
+})
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening: http://localhost:${PORT}`))
